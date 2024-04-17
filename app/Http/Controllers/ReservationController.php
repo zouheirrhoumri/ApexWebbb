ff18c903ae0297ad8f52d2e4fee699ac82bd89e1
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2024-04-17 19:12:52 +0100

    implemented the reservation functionlity for the user

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use App\Models\User;
+use App\Notifications\ReservationNotification;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    // this data will come from a button inside a form that will be submitted
+    public function create(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'service_id' => 'required',
+            'reservation_date' => 'required',
+            'reservation_time' => 'required',
+            'additional_info' => 'required',
+        ]);
+
+        $reservation = new Reservation();
+        $reservation->user_id = $request->user_id;
+        $reservation->service_id = $request->service_id;
+        $reservation->reservation_date = $request->reservation_date;
+        $reservation->reservation_time = $request->reservation_time;
+        $reservation->additional_info = $request->additional_info;
+        $reservation->save();
+
+        $admin = User::where('role', 'admin')->first();
+
+        if ($admin) {
+            $admin->notify(new ReservationNotification($reservation));
+        }
+        
+        return response()->json(['message' => 'Reservation created successfully']);
+    }
+}
---
+++ app/Notifications/ReservationNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Reservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReservationNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    private $reservation;
+
+    public function __construct($reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('A new reservation has been made.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
