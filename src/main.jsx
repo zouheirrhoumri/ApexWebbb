61a399631db0a2f2d8bd4308cd44b50a5c2515bb
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2024-04-23 16:40:04 +0100

    scratching the surface

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Products from './Products.jsx'
 
+import './index.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+
+    <Router>
+      <Routes>
+        <Route index path="/" element={<App />} />
+        <Route path="/products" element={<Products />} />
+
+      </Routes>
+    </Router>
+    {/* <App /> */}
+
   </React.StrictMode>,
 )
