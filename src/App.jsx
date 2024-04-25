ed6d7035e7c203042ec083af610d0e85a090680a
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2024-04-25 21:11:22 +0100

    almost done with hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
+import Hero from './components/Hero';
+
 const App = () => {
 
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
 
         <Header />
+        <Hero />
         
       </div>
 
