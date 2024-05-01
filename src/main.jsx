af946b00248b4ca1841e67212a78df196abd641e
Anass Benfillous <zouheir.rhoumri@gmail.com>
2024-05-02 00:44:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Products from './Products.jsx'
-import Services from './Services.jsx'
-import './index.css'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import ServiceForm from './ServiceForm.jsx'
-import EditService from './EditService.jsx'
-import Register from './Register.jsx'
-import Login from './Login.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Products from "./Products.jsx";
+import Services from "./Services.jsx";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ServiceForm from "./ServiceForm.jsx";
+import EditService from "./EditService.jsx";
+import Register from "./Register.jsx";
+import Login from "./Login.jsx";
+import Statistics from "./Statistics.jsx";
+import { adminChecker, tokenChecker } from "./utils/checker.js";
+import Quote from "./Quote.jsx";
 import Blog from './Blog.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
 
+const isAdmin = adminChecker();
+const isAuthenticated = tokenChecker();
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <Router>
       <Routes>
         <Route index path="/" element={<App />} />
         <Route path="/products" element={<Products />} />
         <Route path="/services" element={<Services />} />
         <Route path="/ServiceForm" element={<ServiceForm />} />
-        <Route path="/editService/:id" element={<EditService />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {isAdmin && isAuthenticated && (
+          <Route path="/editService/:id" element={<EditService />} />
+        )}
+        {!isAuthenticated && <Route path="/login" element={<Login />} />}
+        {!isAuthenticated && <Route path="/register" element={<Register />} />}
+        {isAuthenticated && <Route path="/quote" element={<Quote />} />}
         <Route path="/blog" element={<Blog />} />
-
+        {isAdmin && isAuthenticated && (
+          <Route path="/statistics" element={<Statistics />} />
+        )}
       </Routes>
     </Router>
     {/* <App /> */}
-
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
